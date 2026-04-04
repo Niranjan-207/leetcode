d639d3d10675f95b6cbe2ab3eb5e1bdcc349db0c
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-04-04 23:05:39 +0530

    Create 232-ImplementQueueUsingStacks

---
+++ 232-ImplementQueueUsingStacks/232-ImplementQueueUsingStacks.cpp
@@ -0,0 +1,54 @@
+// Last updated: 4/4/2026, 11:05:38 PM
+class MyQueue {
+public:
+    stack<int> first;
+    stack<int> second;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        first.push(x);
+    }
+    
+    int pop() {
+        while(!first.empty()){
+            second.push(first.top());
+            first.pop();
+        }
+        int ele=second.top();
+        second.pop();
+        while(!second.empty()){
+            first.push(second.top());
+            second.pop();
+        }
+       return ele;
+    }
+    
+    int peek() {
+        while(!first.empty()){
+            second.push(first.top());
+            first.pop();
+        }
+        int ele=second.top();
+        
+        while(!second.empty()){
+            first.push(second.top());
+            second.pop();
+        }
+       return ele;
+    }
+    
+    bool empty() {
+        return first.size()==0;
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
