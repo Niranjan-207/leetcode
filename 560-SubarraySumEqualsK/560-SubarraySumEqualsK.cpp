b9d849a1f97e68c82dfd0fdcda99fabfced74ef3
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-04-04 23:04:26 +0530

    Create 560-SubarraySumEqualsK

---
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
@@ -0,0 +1,17 @@
+// Last updated: 4/4/2026, 11:04:26 PM
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        mp[0]=1;
+        int sum=0,cnt=0;
+        for(int num:nums){
+            sum+=num;
+            if(mp.find(sum-k)!=mp.end()){
+                cnt+=mp[sum-k];
+            }
+            mp[sum]++;
+        }
+        return cnt;
+    }
+};
