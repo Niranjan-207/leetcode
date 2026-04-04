80e1a0fb710118555090333cfbe4f6ea3aacf41f
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-04-04 23:02:05 +0530

    Create 1580-ShuffleTheArray

---
+++ 1580-ShuffleTheArray/1580-ShuffleTheArray.cpp
@@ -0,0 +1,12 @@
+// Last updated: 4/4/2026, 11:02:05 PM
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> arr;
+        for(int i=0;i<n;i++){
+            arr.push_back(nums[i]);
+            arr.push_back(nums[i+n]);
+        }
+        return arr;
+    }
+};
