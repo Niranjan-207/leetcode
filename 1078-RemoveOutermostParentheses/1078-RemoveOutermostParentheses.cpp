8f6640427f4b1762ee0fc6e7647d0bf01f8560e4
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-04-04 23:03:01 +0530

    Create 1078-RemoveOutermostParentheses

---
+++ 1078-RemoveOutermostParentheses/1078-RemoveOutermostParentheses.cpp
@@ -0,0 +1,22 @@
+// Last updated: 4/4/2026, 11:03:00 PM
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans="";
+        stack<char> st;
+        for(char c:s){
+            if(c=='('){
+                if(!st.empty()){
+                    ans+=c;
+                }st.push(c);
+            }
+            else if(c==')'){
+                st.pop();
+                if(!st.empty()){
+                    ans+=c;
+                }
+            }
+        }
+        return ans;
+    }
+};
