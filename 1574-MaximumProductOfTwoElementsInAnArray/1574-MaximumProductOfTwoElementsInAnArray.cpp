081b133bf811e97184cf291dc1f24de036aeea7f
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-04-04 23:02:06 +0530

    Create 1574-MaximumProductOfTwoElementsInAnArray

---
+++ 1574-MaximumProductOfTwoElementsInAnArray/1574-MaximumProductOfTwoElementsInAnArray.cpp
@@ -0,0 +1,9 @@
+// Last updated: 4/4/2026, 11:02:06 PM
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        return max((nums[0]-1)*(nums[1]-1),(nums[n-1]-1)*(nums[n-2]-1));
+    }
+};
