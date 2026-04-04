e3fab1ac84dc0654b1f47986ea6cef903bd1e783
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-04-04 23:01:54 +0530

    Create 1677-MatrixDiagonalSum

---
+++ 1677-MatrixDiagonalSum/1677-MatrixDiagonalSum.cpp
@@ -0,0 +1,16 @@
+// Last updated: 4/4/2026, 11:01:54 PM
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j ||(i+j)==n-1 ){
+                    sum+=mat[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+};
