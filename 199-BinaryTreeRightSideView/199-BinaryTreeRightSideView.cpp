d408be8aede9e63beef4480ffe841670069dac3f
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-04-04 23:06:01 +0530

    Create 199-BinaryTreeRightSideView

---
+++ 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.cpp
@@ -0,0 +1,35 @@
+// Last updated: 4/4/2026, 11:06:01 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if(root==nullptr)   return {};
+        vector<int> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                TreeNode* x=q.front();
+                if(i==n-1){
+                    ans.push_back(x->val);
+                }
+                q.pop();
+                if(x->left!=nullptr)   q.push(x->left);
+                if(x->right!=nullptr)  q.push(x->right);
+
+            }
+        }
+        return ans;
+    }
+};
