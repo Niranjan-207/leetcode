2d7ccff6e7a58d9328303f782d5a63001159dc12
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-04-14 23:51:19 +0530

    File updated at 4/14/2026, 11:51:19 PM

--- 198-HouseRobber/198-HouseRobber.cpp
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -1,22 +1,22 @@
-// Last updated: 4/4/2026, 11:06:02 PM
-class Solution {
-public:
-    int solve(vector<int> nums){
-        int n=nums.size();
-        vector<int> dp(n);
-        dp[0]=nums[0];
-        for(int i=1;i<n;i++){
-            int take=nums[i];
-            if(i>1) take+=dp[i-2];
-
-            int notTake=dp[i-1];
-            dp[i]=max(take,notTake);
-        }
-        return dp[n-1];
-
-    }
-
-    int rob(vector<int>& nums) {
-        return solve(nums);
-    }
-};
+// Last updated: 4/14/2026, 11:51:19 PM
+1class Solution {
+2public:
+3    int solve(vector<int> nums){
+4        int n=nums.size();
+5        vector<int> dp(n);
+6        dp[0]=nums[0];
+7        for(int i=1;i<n;i++){
+8            int take=nums[i];
+9            if(i>1) take+=dp[i-2];
+10
+11            int notTake=dp[i-1];
+12            dp[i]=max(take,notTake);
+13        }
+14        return dp[n-1];
+15
+16    }
+17
+18    int rob(vector<int>& nums) {
+19        return solve(nums);
+20    }
+21};
