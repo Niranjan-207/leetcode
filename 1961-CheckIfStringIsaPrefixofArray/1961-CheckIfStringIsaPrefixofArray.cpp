01b7eda99467e73eee6f6d70ae76f13525556a5c
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-04-18 22:32:26 +0530

    File updated at 4/18/2026, 10:32:25 PM

--- 1961-CheckIfStringIsaPrefixofArray/1961-CheckIfStringIsaPrefixofArray.cpp
+++ 1961-CheckIfStringIsaPrefixofArray/1961-CheckIfStringIsaPrefixofArray.cpp
@@ -1,4 +1,4 @@
-// Last updated: 4/18/2026, 10:31:50 PM
+// Last updated: 4/18/2026, 10:32:25 PM
 1class Solution {
 2public:
 3    bool isPrefixString(string s, vector<string>& words) {
@@ -6,7 +6,8 @@
 5        for(string word:words){
 6            ans+=word;
 7            if(ans.length()==s.length() && s==ans)  return true;
-8        }
-9        return false;
-10    }
-11};
+8            if(s.find(ans)==-1) return false;
+9        }
+10        return false;
+11    }
+12};
