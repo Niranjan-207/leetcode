abd6ffd7b78bb19177655d59316db8dc26a6e131
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-04-18 22:38:51 +0530

    File updated at 4/18/2026, 10:38:50 PM

--- 2255-CountPrefixesofaGivenString/2255-CountPrefixesofaGivenString.cpp
+++ 2255-CountPrefixesofaGivenString/2255-CountPrefixesofaGivenString.cpp
@@ -1,18 +1,13 @@
-// Last updated: 4/18/2026, 10:35:38 PM
+// Last updated: 4/18/2026, 10:38:50 PM
 1class Solution {
 2public:
 3    int countPrefixes(vector<string>& words, string s) {
-4        unordered_map<string,int> mp;
-5        string temp="";
-6        for(string word:words){
-7            mp[word]++;
-8        }
-9        int cnt=0;
-10        temp="";
-11        for(char c:s){
-12            temp+=c;
-13            if(mp.count(temp))  cnt+=mp[temp];
-14        }
-15        return cnt;
-16    }
-17};
+4        int cnt=0;
+5        for(string& word:words){
+6            if(s.length()>=word.length() && s.compare(0,word.size(),word)==0){
+7                cnt++;
+8            }
+9        }
+10        return cnt;
+11    }
+12};
