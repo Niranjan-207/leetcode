39dae7431e58c7dd2f6311aa2ffb31e62d143a4f
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-04-24 22:42:50 +0530

    File updated at 4/24/2026, 10:42:50 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.cpp
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.cpp
@@ -1,28 +1,25 @@
-// Last updated: 4/4/2026, 11:08:05 PM
-class Solution {
-public:
-    string check(int l,int r,string& s){
-        while(l>=0 && r<s.length() &&s[l]==s[r]){
-            l--;r++;
-        }
-        return s.substr(l+1,r-l-1);
-    }
-    string longestPalindrome(string s) {
-        int n=s.length();
-        
-        string ans="";
-        for(int i=0;i<n;i++){
-            string temp=check(i,i,s);
-            
-            if(ans.length()<temp.length()){
-                ans=temp;
-            }
-            temp=check(i,i+1,s);
-            if(ans.length()<temp.length()){
-                ans=temp;
-            }
-        }
-        return ans;
-    }
-    
-};
+// Last updated: 4/24/2026, 10:42:50 PM
+1class Solution {
+2public:
+3    string check(string& s,int l,int r){
+4        int n=s.length();
+5        while(l>=0 && r<n && s[l]==s[r]){
+6            l--;r++;
+7        }
+8        return s.substr(l+1,r-l-1);
+9    }
+10
+11    string longestPalindrome(string s) {
+12        string ans="";
+13        int n=s.length();
+14
+15        for(int i=0;i<n;i++){
+16            string cur=check(s,i,i);
+17            if(cur.length()>ans.length()) ans=cur;
+18
+19            cur=check(s,i,i+1);
+20            if(cur.length()>ans.length()) ans=cur;
+21        }
+22        return ans;
+23    }
+24};
