b206f771379ed70576f7b2d4a358f35764778f48
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-04-27 00:51:14 +0530

    File updated at 4/27/2026, 12:51:14 AM

--- 1502-CanMakeArithmeticProgressionFromSequence/1502-CanMakeArithmeticProgressionFromSequence.cpp
+++ 1502-CanMakeArithmeticProgressionFromSequence/1502-CanMakeArithmeticProgressionFromSequence.cpp
@@ -1,13 +1,14 @@
-// Last updated: 4/27/2026, 12:48:32 AM
+// Last updated: 4/27/2026, 12:51:14 AM
 1class Solution {
 2public:
-3    bool canMakeArithmeticProgression(vector<int>& arr) {
-4        int n=arr.size();
-5        sort(begin(arr),end(arr));
-6        int dif=arr[1]-arr[0];
-7        for(int i=1;i<n;i++){
-8            if(arr[i]-arr[i-1]!=dif)    return false;
-9        }
-10        return true;
-11    }
-12};
+3    int pivotInteger(int n) {
+4        int tot=(n*(n+1))/2;
+5        int cur=0;
+6        for(int i=1;i<=n;i++){
+7            cur+=i;
+8            int ri=tot-cur+i;
+9            if(cur==ri) return i;
+10        }
+11        return -1;
+12    }
+13};
