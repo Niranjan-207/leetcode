b728c0c6daabe9c14a72fba1d047e21b4f20df0d
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-03 01:23:31 +0530

    File updated at 5/3/2026, 1:23:31 AM

--- 494-TargetSum/494-TargetSum.cpp
+++ 494-TargetSum/494-TargetSum.cpp
@@ -1,19 +1,26 @@
-// Last updated: 4/4/2026, 11:04:39 PM
-class Solution {
-public:
-int cnt=0;
-    void solve(vector<int>& nums,int temp,int idx,int& target){
-        if(idx==nums.size()){
-            if(temp==target)    cnt++;
-            return;
-        }
-
-        solve(nums,temp-nums[idx],idx+1,target);
-        solve(nums,temp+nums[idx],idx+1,target);
-    }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        solve(nums,0,0,target);
-        
-        return cnt;
-    }
-};
+// Last updated: 5/3/2026, 1:23:31 AM
+1class Solution {
+2public:
+3    int f(int idx,int target,vector<int>& nums,vector<vector<int>>& dp,int& tot){
+4        if(idx==0){
+5            if(target==0 && nums[idx]==0)   return 2;
+6            if(target==nums[0] || target==-nums[0]) return 1;
+7            return 0;
+8        }
+9        if (target > tot || target < -tot) return 0;
+10
+11        if(dp[idx][target+tot]!=-1) return dp[idx][target+tot];
+12
+13        int m=f(idx-1,target-nums[idx],nums,dp,tot);
+14        int p=f(idx-1,target+nums[idx],nums,dp,tot);
+15
+16        return dp[idx][target+tot]=m+p;
+17    }
+18
+19    int findTargetSumWays(vector<int>& nums, int target) {
+20        int tot=accumulate(begin(nums),end(nums),0);
+21        int n=nums.size();
+22        vector<vector<int>> dp(n,vector<int> (2*tot+1,-1));
+23        return f(n-1,target,nums,dp,tot);
+24    }
+25};
