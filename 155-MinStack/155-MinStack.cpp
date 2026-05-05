8024d5196f3282a71fe7ea0cc882d356f1c6b658
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-05 12:39:47 +0530

    File updated at 5/5/2026, 12:39:47 PM

--- 155-MinStack/155-MinStack.cpp
+++ 155-MinStack/155-MinStack.cpp
@@ -1,39 +1,40 @@
-// Last updated: 4/4/2026, 11:06:21 PM
-class MinStack {
-public:
-    stack<int> st,mst;
-    MinStack() {
-        
-    }
-    
-    void push(int val) {
-        st.push(val);
-        if(mst.empty()){
-            mst.push(val);
-            return;
-        }
-        mst.push(min(mst.top(),val));
-    }
-    
-    void pop() {
-        mst.pop();
-        st.pop();
-    }
-    
-    int top() {
-        return st.top();
-    }
-    
-    int getMin() {
-        return mst.top();
-    }
-};
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
+// Last updated: 5/5/2026, 12:39:47 PM
+1class MinStack {
+2public:
+3    stack<int> st,mst;
+4    MinStack() {
+5        
+6    }
+7    
+8    void push(int val) {
+9        if(st.empty()){
+10            st.push(val);
+11            mst.push(val);
+12            return;
+13        }
+14        st.push(val);
+15        mst.push(min(mst.top(),val));
+16    }
+17    
+18    void pop() {
+19        st.pop();
+20        mst.pop();
+21    }
+22    
+23    int top() {
+24        return st.top();
+25    }
+26    
+27    int getMin() {
+28        return mst.top();
+29    }
+30};
+31
+32/**
+33 * Your MinStack object will be instantiated and called as such:
+34 * MinStack* obj = new MinStack();
+35 * obj->push(val);
+36 * obj->pop();
+37 * int param_3 = obj->top();
+38 * int param_4 = obj->getMin();
+39 */
