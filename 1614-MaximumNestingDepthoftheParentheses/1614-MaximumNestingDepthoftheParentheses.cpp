e4f6bd194dc72452d0a0ee45c284ee840109d78f
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-05 10:43:52 +0530

    File updated at 5/5/2026, 10:43:52 AM

--- 1614-MaximumNestingDepthoftheParentheses/1614-MaximumNestingDepthoftheParentheses.cpp
+++ 1614-MaximumNestingDepthoftheParentheses/1614-MaximumNestingDepthoftheParentheses.cpp
@@ -1,9 +1,9 @@
-// Last updated: 5/5/2026, 10:43:40 AM
+// Last updated: 5/5/2026, 10:43:52 AM
 1class Solution {
 2public:
 3    int minAddToMakeValid(string s) {
 4        int open=0,extra=0;
-5        for(char c:s){
+5        for(char& c:s){
 6            if(c=='(')  open++;
 7            else{
 8                if(open>0){
