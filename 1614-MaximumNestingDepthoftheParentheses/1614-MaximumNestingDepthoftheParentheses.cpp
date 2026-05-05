f9f67ebd209dfec8a85df6784a53bf3245554fa3
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-05 10:10:48 +0530

    File updated at 5/5/2026, 10:10:48 AM

--- 1614-MaximumNestingDepthoftheParentheses/1614-MaximumNestingDepthoftheParentheses.cpp
+++ 1614-MaximumNestingDepthoftheParentheses/1614-MaximumNestingDepthoftheParentheses.cpp
@@ -1,10 +1,10 @@
-// Last updated: 5/5/2026, 10:10:32 AM
+// Last updated: 5/5/2026, 10:10:48 AM
 1class Solution {
 2public:
 3    int maxDepth(string s) {
 4        stack<char> st;
 5        int len=0;
-6        for(char c:s){
+6        for(char& c:s){
 7            if(c=='(')  st.push(c);
 8            else if(c==')'){
 9                int clen=st.size();
