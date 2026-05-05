60bc63af9ed858057ecf13c9a630874218a8e851
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-05 22:09:48 +0530

    File updated at 5/5/2026, 10:09:47 PM

--- 1335-MinimumDifficultyofaJobSchedule/1335-MinimumDifficultyofaJobSchedule.cpp
+++ 1335-MinimumDifficultyofaJobSchedule/1335-MinimumDifficultyofaJobSchedule.cpp
@@ -1,27 +1,28 @@
-// Last updated: 5/5/2026, 9:06:29 PM
+// Last updated: 5/5/2026, 10:09:47 PM
 1class Solution {
 2public:
-3    unordered_map<string,int> dp;
-4    string encode(int idx,int ma,int cd){
-5        return (to_string(idx)+'#'+to_string(ma)+'#'+to_string(cd));
-6    }
-7    int f(int idx,int ma,vector<int>& arr,int cd,int d){
-8        if(cd>d)    return 1e9;
-9        if(idx==arr.size() && cd==d)    return ma;
-10        if(idx==arr.size()) return 1e9;
-11
-12        string key=encode(idx,ma,cd);
-13        if(dp.count(key)) return dp[key];
-14        int newDay=ma+f(idx+1,arr[idx],arr,cd+1,d);
-15        int sameDay=f(idx+1,max(ma,arr[idx]),arr,cd,d);
-16        return dp[key]=min(newDay,sameDay);
-17    }
-18
-19    int minDifficulty(vector<int>& jobDifficulty, int d) {
-20        int n=jobDifficulty.size();
-21        if(n<d) return -1;
-22        if(n==d)    return accumulate(begin(jobDifficulty),end(jobDifficulty),0);
-23        
-24        return f(1,jobDifficulty[0],jobDifficulty,1,d);   //idx,max,arr,cd,d    
-25    }
-26};
+3   
+4   
+5    int f(int idx,int& n,vector<int>& arr,int d,vector<vector<int>>& dp){
+6       if(d==1){
+7            return *max_element(begin(arr)+idx,end(arr));
+8       }
+9        if(dp[idx][d]!=-1)  return dp[idx][d];
+10       int finalValue=INT_MAX;
+11       int curMax=INT_MIN;
+12       for(int i=idx;i<=n-d;i++){
+13        curMax=max(curMax,arr[i]);
+14        int segAns=curMax+f(i+1,n,arr,d-1,dp);
+15        finalValue=min(finalValue,segAns);
+16       }
+17       return dp[idx][d]=finalValue;
+18    }
+19
+20    int minDifficulty(vector<int>& jobDifficulty, int d) {
+21        int n=jobDifficulty.size();
+22        if(n<d) return -1;
+23        if(n==d)    return accumulate(begin(jobDifficulty),end(jobDifficulty),0);
+24        vector<vector<int>> dp(n,vector<int>(d+1,-1));
+25        return f(0,n,jobDifficulty,d,dp);   //idx,arr,cd,d    
+26    }
+27};
