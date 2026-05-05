d4e2275f55e73e06935590b1ac6c4829346e93fc
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-05 12:02:11 +0530

    File updated at 5/5/2026, 12:02:10 PM

--- 1047-RemoveAllAdjacentDuplicatesInString/1047-RemoveAllAdjacentDuplicatesInString.cpp
+++ 1047-RemoveAllAdjacentDuplicatesInString/1047-RemoveAllAdjacentDuplicatesInString.cpp
@@ -1,21 +1,15 @@
-// Last updated: 5/5/2026, 12:00:38 PM
-1class Solution {
-2public:
-3    string removeDuplicates(string s) {
-4        stack<char> st;
-5        for(char c:s){
-6            if(st.empty())  st.push(c);
-7            else{
-8                if(st.top()==c) st.pop();
-9                else    st.push(c);
-10            }
-11        }
-12        string ans="";
-13        while(!st.empty()){
-14            ans+=st.top();
-15            st.pop();
-16        }
-17        reverse(begin(ans),end(ans));
-18        return ans;
-19    }
-20};
+// Last updated: 5/5/2026, 12:02:10 PM
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res = "";
+        for (char c : s) {
+            if (!res.empty() && res.back() == c) {
+                res.pop_back();
+            } else {
+                res.push_back(c);
+            }
+        }
+        return res;
+    }
+};
