d08af849571ca7b9a0fbb6d67a5311f8401f20e0
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-05 10:42:37 +0530

    File updated at 5/5/2026, 10:42:37 AM

--- 1614-MaximumNestingDepthoftheParentheses/1614-MaximumNestingDepthoftheParentheses.cpp
+++ 1614-MaximumNestingDepthoftheParentheses/1614-MaximumNestingDepthoftheParentheses.cpp
@@ -1,22 +1,18 @@
-// Last updated: 5/5/2026, 10:37:02 AM
+// Last updated: 5/5/2026, 10:42:37 AM
 1class Solution {
 2public:
 3    int minAddToMakeValid(string s) {
-4        stack<char> st;
+4        int open=0,moves=0;
 5        for(char c:s){
-6            if(c=='(')  st.push(c);
+6            if(c=='(')  open++;
 7            else{
-8                if(st.empty())  st.push(c);
-9                else{
-10                    char to=st.top();
-11                    if(c==')'  && to=='('){
-12                        st.pop();
-13                    }else{
-14                        st.push(c);
-15                    }
-16                }
-17            }
-18        }
-19        return st.size();
-20    }
-21};
+8                if(open>0){
+9                    open--;
+10                }else{
+11                    moves++;
+12                }
+13            }
+14        }
+15        return open+moves;
+16    }
+17};
