93086fc41a9af4e12e07b8e75082b6fa7cc7bb6c
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-06 14:32:28 +0530

    File updated at 5/6/2026, 2:32:28 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.cpp
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.cpp
@@ -1,4 +1,4 @@
-// Last updated: 5/6/2026, 2:02:17 PM
+// Last updated: 5/6/2026, 2:32:28 PM
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -12,18 +12,31 @@
 11 */
 12class Solution {
 13public:
-14    TreeNode* solve(TreeNode* root){
-15        if(!root)   return nullptr;
-16        TreeNode* le=solve(root->left);
-17        TreeNode* ri=solve(root->right);
-18        
-19        root->left=ri;
-20        root->right=le;
-21        return root;
-22    }
-23
-24    TreeNode* invertTree(TreeNode* root) {
-25        if(!root)   return root;
-26        return solve(root);
-27    }
-28};
+14    TreeNode* reverseOddLevels(TreeNode* root) {
+15        if(!root)   return root;
+16        queue<TreeNode*> q;
+17        q.push(root);
+18        int lev=0;
+19        while(!q.empty()){
+20            int n=q.size();
+21            vector<TreeNode*> nodes;
+22            for(int i=0;i<n;i++){
+23                TreeNode* cur=q.front();
+24                q.pop();
+25                nodes.push_back(cur);
+26
+27                if(cur->left)   q.push(cur->left);
+28                if(cur->right)  q.push(cur->right);
+29            }
+30
+31            if(lev%2==1){
+32                int l=0,r=nodes.size()-1;
+33                while(l<r){
+34                    swap(nodes[l++]->val,nodes[r--]->val);
+35                }
+36            }
+37            lev++;
+38        }
+39        return root;
+40    }
+41};
