8fd157050691b5be69f0d9d406819b00c593e0ae
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-07 12:02:04 +0530

    File updated at 5/7/2026, 12:02:04 PM

--- 1926-NearestExitfromEntranceinMaze/1926-NearestExitfromEntranceinMaze.cpp
+++ 1926-NearestExitfromEntranceinMaze/1926-NearestExitfromEntranceinMaze.cpp
@@ -1,42 +1,32 @@
-// Last updated: 5/7/2026, 12:00:43 PM
-1class Solution {
-2public:
-3    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-4        int n=maze.size();
-5        int m=maze[0].size();
-6        vector<vector<bool>> vis(n,vector<bool>(m,false));
-7        vector<int> X={1,0,-1,0};
-8        vector<int> Y={0,1,0,-1};
-9
-10        int cnt=0;
-11        queue<pair<int,int>> q;
-12        int ex=entrance[0],ey=entrance[1];
-13        vis[ex][ey]=true;
-14        q.push({ex,ey});
-15
-16        while(!q.empty()){
-17            int qn=q.size();
-18            
-19            for(int i=0;i<qn;i++){
-20                cout<<cnt<<" ";
-21                auto [x,y]=q.front();
-22                q.pop();
-23
-24                if((x==0 || x==n-1 || y==0 || y==m-1) && (x!=ex || y!=ey) )    return cnt;
-25
-26                for(int i=0;i<4;i++){
-27                    int nx=x+X[i];
-28                    int ny=y+Y[i];
-29
-30                    if(nx<n && nx>=0 && ny<m && ny>=0 && !vis[nx][ny] && maze[nx][ny]=='.'){
-31                        q.push({nx,ny});
-32                        vis[nx][ny]=true;
-33                    }
-34                }
-35            }
-36            cnt++;
-37            
-38        }
-39        return -1;
-40    }
-41};
+// Last updated: 5/7/2026, 12:02:04 PM
+class Solution {
+ public:
+  int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+    constexpr int kDirs[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    const int m = maze.size();
+    const int n = maze[0].size();
+    queue<pair<int, int>> q{{{entrance[0], entrance[1]}}};
+    vector<vector<bool>> seen(m, vector<bool>(n));
+    seen[entrance[0]][entrance[1]] = true;
+
+    for (int step = 1; !q.empty(); ++step)
+      for (int sz = q.size(); sz > 0; --sz) {
+        const auto [i, j] = q.front();
+        q.pop();
+        for (const auto& [dx, dy] : kDirs) {
+          const int x = i + dx;
+          const int y = j + dy;
+          if (x < 0 || x == m || y < 0 || y == n)
+            continue;
+          if (seen[x][y] || maze[x][y] == '+')
+            continue;
+          if (x == 0 || x == m - 1 || y == 0 || y == n - 1)
+            return step;
+          q.emplace(x, y);
+          seen[x][y] = true;
+        }
+      }
+
+    return -1;
+  }
+};
