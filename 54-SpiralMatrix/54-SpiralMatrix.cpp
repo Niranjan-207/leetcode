ec04aa56530f47223001a9907468025de512715d
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-09 12:17:46 +0530

    File updated at 5/9/2026, 12:17:45 PM

--- 54-SpiralMatrix/54-SpiralMatrix.cpp
+++ 54-SpiralMatrix/54-SpiralMatrix.cpp
@@ -1,38 +1,26 @@
-// Last updated: 5/9/2026, 12:07:43 PM
+// Last updated: 5/9/2026, 12:17:45 PM
 1class Solution {
 2public:
 3    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-4        int rowStart=0,rowEnd=matrix.size()-1;
-5        int colStart=0,colEnd=matrix[0].size()-1;
-6
-7        vector<int> ans;
-8        while(rowStart<rowEnd && colStart<colEnd){
-9            for(int i=colStart;i<=colEnd;i++){
-10                ans.push_back(matrix[rowStart][i]);
-11            }
-12            rowStart++;
-13            for(int i=rowStart;i<=rowEnd;i++){
-14                ans.push_back(matrix[i][colEnd]);
-15            }
-16            colEnd--;
-17            for(int i=colEnd;i>=colStart;i--){
-18                ans.push_back(matrix[rowEnd][i]);
-19            }
-20            rowEnd--;
-21            for(int i=rowEnd;i>=rowStart;i--){
-22                ans.push_back(matrix[i][colStart]);
-23            }
-24            colStart++;
-25        }
-26        if(rowStart==rowEnd){
-27            for(int i=colStart;i<=colEnd;i++){
-28                ans.push_back(matrix[rowEnd][i]);
-29            }
-30        }else if(colEnd==colStart){
-31            for(int i=rowStart;i<=rowEnd;i++){
-32                ans.push_back(matrix[i][colEnd]);
-33            }
-34        }
-35        return ans;
-36    }
-37};
+4        vector<vector<int>> dir={{0,1},{1,0},{0,-1},{-1,0}};
+5
+6        vector<int> ans;
+7        int n=matrix.size();
+8        int m=matrix[0].size();
+9
+10        int cdir=0;
+11        int cr=0,cc=0;
+12        while(ans.size()<(n*m)){
+13            ans.push_back(matrix[cr][cc]);
+14            matrix[cr][cc]=10000;
+15            int nr=cr+dir[cdir][0];
+16            int nc=cc+dir[cdir][1];
+17            if(nr==n || nc==m || nc<0 || nr<0 || matrix[nr][nc]==10000)
+18                cdir=(cdir+1)%4;
+19
+20            cr+=dir[cdir][0];
+21            cc+=dir[cdir][1];
+22        }
+23        return ans;
+24    }
+25};
