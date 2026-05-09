2060fbf18e1fd3e3b142bedd510fd07141053fa3
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-09 14:08:31 +0530

    File updated at 5/9/2026, 2:08:31 PM

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.cpp
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.cpp
@@ -1,37 +1,15 @@
-// Last updated: 5/2/2026, 2:21:37 PM
-#include <bitset>
-#include <numeric>
-
-static const int speedup = []() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    return 0;
-}();
-
-class Solution {
-public:
-    bool canPartition(vector<int>& nums) {
-        int total_sum = accumulate(nums.begin(), nums.end(), 0);
-        
-        // If the total sum is odd, we cannot split it into two equal integer sums
-        if (total_sum % 2 != 0) return false;
-        
-        int target = total_sum / 2;
-        
-        // Constraints: nums.length <= 200, nums[i] <= 100. 
-        // Max target sum = (200 * 100) / 2 = 10000.
-        bitset<10001> dp;
-        dp[0] = 1; // Base case: a sum of 0 is always possible
-        
-        for (int num : nums) {
-            // Left shift the bitset by 'num' and OR it with the original
-            // This marks all new possible sums reachable by adding 'num'
-            dp |= (dp << num);
-            
-            // Early exit if we already reached the target
-            if (dp[target]) return true;
-        }
-        
-        return dp[target];
-    }
-};
+// Last updated: 5/9/2026, 2:08:31 PM
+1class Solution {
+2public:
+3    bool canPartition(vector<int>& nums) {
+4        int sum=accumulate(begin(nums),end(nums),0);
+5        if(sum&1)   return false;
+6        bitset<100000> dp;
+7        dp[0]=1;
+8        for(int num:nums){
+9            dp=dp|(dp<<num);
+10            if(dp[sum/2])   return true;
+11        }
+12        return false;
+13    }
+14};
