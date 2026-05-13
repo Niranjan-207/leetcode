4dd0e49ffc51a471ffd509700cbd52e46cad1449
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-14 00:22:36 +0530

    File updated at 5/14/2026, 12:22:36 AM

--- 1920-BuildArrayfromPermutation/1920-BuildArrayfromPermutation.cpp
+++ 1920-BuildArrayfromPermutation/1920-BuildArrayfromPermutation.cpp
@@ -1,18 +1,27 @@
-// Last updated: 5/14/2026, 12:15:22 AM
-1class Solution {
+// Last updated: 5/14/2026, 12:22:36 AM
+1class MyCalendarThree {
 2public:
-3    bool carPooling(vector<vector<int>>& trips, int capacity) {
-4        map<int,int> mp;
-5        for(vector<int>& trip:trips){
-6            int n=trip[0],l=trip[1],r=trip[2];
-7            mp[l]+=n;
-8            mp[r]-=n;
-9        }
-10        int cap=0;
-11        for(auto& [ran,delta]:mp){
-12            cap+=delta;
-13            if(cap>capacity)    return false;
-14        }
-15        return true;
-16    }
-17};
+3map<int,int> mp;
+4    MyCalendarThree() {
+5        
+6    }
+7    
+8    int book(int startTime, int endTime) {
+9        mp[startTime]+=1;
+10        mp[endTime]-=1;
+11
+12        int k=0;
+13        int overLap=0;
+14        for(auto& [day,delta]:mp){
+15            overLap+=delta;
+16            k=max(k,overLap);
+17        }
+18        return k;
+19    }
+20};
+21
+22/**
+23 * Your MyCalendarThree object will be instantiated and called as such:
+24 * MyCalendarThree* obj = new MyCalendarThree();
+25 * int param_1 = obj->book(startTime,endTime);
+26 */
