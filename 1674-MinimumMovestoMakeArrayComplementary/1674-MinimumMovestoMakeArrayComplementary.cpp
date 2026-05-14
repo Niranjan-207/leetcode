e262a6c11df1f51ad532f3118f61711a7e1cb79e
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-14 20:29:17 +0530

    File updated at 5/14/2026, 8:29:16 PM

--- 1674-MinimumMovestoMakeArrayComplementary/1674-MinimumMovestoMakeArrayComplementary.cpp
+++ 1674-MinimumMovestoMakeArrayComplementary/1674-MinimumMovestoMakeArrayComplementary.cpp
@@ -1,25 +1,29 @@
-// Last updated: 5/14/2026, 12:45:53 AM
+// Last updated: 5/14/2026, 8:29:16 PM
 1class Solution {
 2public:
 3    int minMoves(vector<int>& nums, int limit) {
-4        int n = nums.size();
-5        vector<int> dp(2 * limit + 2, 0);
-6        for (int i = 0; i < n / 2; i++) {
-7            int mini = min(nums[i], nums[n - 1 - i]);
-8            int maxi = max(nums[i], nums[n - 1 - i]);
-9            dp[2] += 2;
-10            dp[mini + 1] -= 1;
-11            dp[mini + maxi] -= 1;
-12            dp[mini + maxi + 1] += 1;
-13            dp[maxi + limit + 1] += 1;
-14        }
-15        int res = n;
-16        int moves = 0;
-17        for (int c = 2; c <= 2 * limit; c++) {
-18            moves += dp[c];
-19            res = min(res, moves);
-20        }
-21
-22        return res;
-23    }
-24};
+4        int n=nums.size();
+5        vector<int> dif(2*limit+2,0);
+6        for(int i=0;i<(n/2);i++){
+7            dif[2]+=2;
+8            dif[2*limit+1]-=2;
+9
+10            int a=nums[i];
+11            int b=nums[n-i-1];
+12            int s=a+b;
+13
+14            int l=min(a,b)+1;
+15            int r=max(a,b)+limit;
+16
+17            dif[l]+=(-1);
+18            dif[r+1]-=(-1);
+19            dif[s]+=(-1);
+20            dif[s+1]-=(-1);
+21        }
+22
+23        for(int i=1;i<dif.size();i++){
+24            dif[i]+=dif[i-1];
+25        }
+26        return *min_element(begin(dif)+2,end(dif)-1);
+27    }
+28};
