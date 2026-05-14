c0001f3559d55ae19e336e804e82949ff0eea9da
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-05-14 19:26:11 +0530

    File updated at 5/14/2026, 7:26:10 PM

--- 1589-MaximumSumObtainedofAnyPermutation/1589-MaximumSumObtainedofAnyPermutation.cpp
+++ 1589-MaximumSumObtainedofAnyPermutation/1589-MaximumSumObtainedofAnyPermutation.cpp
@@ -1,30 +1,28 @@
-// Last updated: 5/14/2026, 6:25:49 PM
+// Last updated: 5/14/2026, 7:26:10 PM
 1class Solution {
 2public:
-3    
-4
-5    int maxSumRangeQuery(vector<int>& nums, vector<vector<int>>& requests) {
-6        int mod=1e9+7;
-7        int n=nums.size();
-8        vector<int> fre(n);
-9        for(vector<int>& request:requests){
-10            fre[request[0]]+=1;
-11            if(request[1]+1<n){
-12                fre[request[1]+1]-=1;
-13            }
+3    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+4        //map<int,int> mp;
+5        
+6        vector<int> fre(n+1,0);
+7        for(vector<int> booking:bookings){
+8            fre[booking[0]-1]+=booking[2];
+9            //if(booking[1]+1<n)
+10                fre[booking[1]]-=booking[2];
+11        }
+12        for(int i=1;i<=n;i++){
+13            fre[i]+=fre[i-1];
 14        }
-15        sort(rbegin(nums),rend(nums));
-16        for(int i=1;i<n;i++){
-17            fre[i]=fre[i-1]+fre[i];
-18        }
-19        sort(rbegin(fre),rend(fre));
-20        long long sum=0;
-21
-22        for(int i=0;i<n;i++){
-23            sum=(sum+(fre[i]*1ll*nums[i])%mod)%mod;
-24        }
-25
-26        return (int)sum;
-27        
-28    }
-29};
+15
+16        vector<int> ans(n);
+17        for(int i=0;i<n;i++){
+18            ans[i]=fre[i];
+19        }
+20        //int cnt=0;
+21        // for(auto& [idx,delta]:mp){
+22        //     cnt+=delta;
+23        //     ans.push_back(cnt);
+24        // }
+25        return ans;
+26    }
+27};
