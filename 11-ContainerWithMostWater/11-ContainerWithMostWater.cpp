24595eef1e97e80f1d8e2f205e012f593a7b41db
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-06-30 01:19:53 +0530

    File updated at 6/30/2026, 1:19:52 AM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.cpp
@@ -1,17 +1,15 @@
-// Last updated: 4/4/2026, 11:08:01 PM
-class Solution {
-public:
-    int maxArea(vector<int>& height) {
-        int ans=INT_MIN;
-        int l=0,r=height.size()-1;
-        while(l<r){
-            ans=max(ans,(r-l)*min(height[l],height[r]));
-            if(height[l]<height[r]){
-                l++;
-            }else{
-                r--;
-            }
-        }
-        return ans;
-    }
-};
+// Last updated: 6/30/2026, 1:19:52 AM
+1class Solution {
+2public:
+3    int maxArea(vector<int>& height) {
+4        int l=0,r=height.size()-1;
+5
+6        int ans=0;
+7        while(l<r){
+8            ans=max(ans,(r-l)*min(height[l],height[r]));
+9            if(height[l]<height[r]) l++;
+10            else    r--;
+11        }
+12        return ans;
+13    }
+14};
