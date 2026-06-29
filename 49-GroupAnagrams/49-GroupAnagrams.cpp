5080eafbf3715794c7620e4bc5a799b46e5dff4f
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-06-29 23:01:32 +0530

    File updated at 6/29/2026, 11:01:32 PM

--- 49-GroupAnagrams/49-GroupAnagrams.cpp
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -1,17 +1,18 @@
-// Last updated: 4/4/2026, 11:07:30 PM
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>> mp;
-        for(string& s:strs){
-            string temp=s;
-            sort(temp.begin(),temp.end());
-            mp[temp].push_back(s);
-        }
-        vector<vector<string>> ans;
-        for(auto& [s,vec]:mp){
-            ans.push_back(vec);
-        }
-        return ans;
-    }
-};
+// Last updated: 6/29/2026, 11:01:32 PM
+1class Solution {
+2public:
+3    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+4        unordered_map<string,vector<string>> mp;
+5        for(string s:strs){
+6            string temp=s;
+7            sort(begin(temp),end(temp));
+8            mp[temp].push_back(s);
+9        }
+10
+11        vector<vector<string>> ans;
+12        for(auto [key,val]:mp){
+13            ans.push_back(val);
+14        }
+15        return ans;
+16    }
+17};
