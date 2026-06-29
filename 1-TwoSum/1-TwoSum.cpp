0328f5e2f17eadf25352016ae245d7ba21ea5af8
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-06-29 22:58:05 +0530

    File updated at 6/29/2026, 10:58:04 PM

--- 1-TwoSum/1-TwoSum.cpp
+++ 1-TwoSum/1-TwoSum.cpp
@@ -1,18 +1,16 @@
-// Last updated: 4/4/2026, 11:08:08 PM
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<int> ans(2);
-        unordered_map<int,int> mp;
-        for(int i=0;i<n;i++){
-            int req=target-nums[i];
-            if(mp.find(req)!=mp.end()){
-                ans[0]=mp[req];
-                ans[1]=i;
-            }
-            mp[nums[i]]=i;
-        }
-        return ans;
-    }
-};
+// Last updated: 6/29/2026, 10:58:04 PM
+1class Solution {
+2public:
+3    vector<int> twoSum(vector<int>& nums, int target) {
+4        unordered_map<int,int> mp;
+5        for(int i=0;i<nums.size();i++){
+6            int num=nums[i];
+7            int need=target-num;
+8            if(mp.count(need)){
+9                return {mp[need],i};
+10            }
+11            mp[num]=i;
+12        }
+13        return {};
+14    }
+15};
