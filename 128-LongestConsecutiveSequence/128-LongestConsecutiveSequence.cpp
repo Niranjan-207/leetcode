e28d4653ee416f62d44c51b37c495c1863ebfee6
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-06-29 23:26:56 +0530

    File updated at 6/29/2026, 11:26:56 PM

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.cpp
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.cpp
@@ -1,23 +1,20 @@
-// Last updated: 4/4/2026, 11:06:38 PM
-class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
-        if(nums.size()==0)  return 0;
-        set<int> se(nums.begin(),nums.end());
-        int len=1;
-        int ans=INT_MIN;
-
-        for(int num:se){
-            int ne=num+1;
-            if(se.find(ne)!=se.end()){
-                len++;
-            }else{
-                ans=max(len,ans);
-                len=1;
-            }
-        }
-
-        return ans;
-
-    }
-};
+// Last updated: 6/29/2026, 11:26:56 PM
+1class Solution {
+2public:
+3    int longestConsecutive(vector<int>& nums) {
+4        set<int> se(begin(nums),end(nums));
+5
+6        int ans=0;
+7        for(int num:se){
+8            if(!se.count(num-1)){
+9                int cur=num+1;
+10
+11                int len=1;
+12                while(se.count(cur++))  len++;
+13
+14                ans=max(ans,len);
+15            }
+16        }
+17        return ans;
+18    }
+19};
