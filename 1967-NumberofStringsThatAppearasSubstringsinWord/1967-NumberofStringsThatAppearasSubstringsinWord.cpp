1e1f02219fa816defcd3a8a6819ed902ea7c9937
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-06-29 15:56:13 +0530

    Create 1967-NumberofStringsThatAppearasSubstringsinWord

---
+++ 1967-NumberofStringsThatAppearasSubstringsinWord/1967-NumberofStringsThatAppearasSubstringsinWord.cpp
@@ -0,0 +1,11 @@
+// Last updated: 6/29/2026, 3:56:13 PM
+1class Solution {
+2public:
+3    int numOfStrings(vector<string>& patterns, string word) {
+4        int cnt=0;
+5        for(string s : patterns){
+6            if(word.find(s)!=string::npos) cnt++;
+7        }
+8        return cnt;
+9    }
+10};
