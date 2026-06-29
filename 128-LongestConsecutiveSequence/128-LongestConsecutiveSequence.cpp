345e6b77741f48408c2c51dc6dc7eab818a0147e
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-06-29 23:27:28 +0530

    File updated at 6/29/2026, 11:27:27 PM

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.cpp
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.cpp
@@ -1,20 +1,43 @@
-// Last updated: 6/29/2026, 11:26:56 PM
-1class Solution {
-2public:
-3    int longestConsecutive(vector<int>& nums) {
-4        set<int> se(begin(nums),end(nums));
-5
-6        int ans=0;
-7        for(int num:se){
-8            if(!se.count(num-1)){
-9                int cur=num+1;
-10
-11                int len=1;
-12                while(se.count(cur++))  len++;
-13
-14                ans=max(ans,len);
-15            }
-16        }
-17        return ans;
-18    }
-19};
+// Last updated: 6/29/2026, 11:27:27 PM
+class Solution {
+public:
+    static constexpr int MAX_RANGE = 16 << 20;
+    int solvewithSet(vector<int>& nums) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        int best = 0;
+        for (int n : s) {
+            if (s.count(n - 1))
+                continue;
+            int len = 1;
+            while (s.count(n + len))
+                len++;
+            best = max(best, len);
+        }
+        return best;
+    }
+    int longestConsecutive(vector<int>& nums) {
+        if (nums.empty())
+            return 0;
+        int mn = INT_MAX, mx = INT_MIN;
+        for (int n : nums) {
+            mn = min(n, mn);
+            mx = max(n, mx);
+        }
+        int r = mx - mn + 1;
+        if (r > MAX_RANGE) {
+            return solvewithSet(nums);
+        }
+        vector<bool> seen(r, false);
+        for (int n : nums) {
+            seen[n - mn] = true;
+        }
+        int cur = 0, best = 0;
+        for (int i = 0; i < r; i++) {
+            if (seen[i])
+                best = max(best, ++cur);
+            else
+                cur = 0;
+        }
+        return best;
+    }
+};
