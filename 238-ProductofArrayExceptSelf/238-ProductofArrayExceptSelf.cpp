8ee300e090f9bb9cfc893f09abec5b9e4519ba65
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-06-29 23:20:11 +0530

    File updated at 6/29/2026, 11:20:11 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.cpp
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.cpp
@@ -1,30 +1,22 @@
-// Last updated: 6/29/2026, 11:16:50 PM
+// Last updated: 6/29/2026, 11:20:11 PM
 1class Solution {
 2public:
 3    vector<int> productExceptSelf(vector<int>& nums) {
-4        long long pro=1;
-5        int cnt0=0;
-6        for(int num:nums){
-7            if(num==0)  cnt0++;
-8            else    pro*=num;
-9        }
-10
-11        int n=nums.size();
-12        vector<int> ans(n,0);
-13        if(cnt0>1){
-14            return ans;
-15        }
-16
-17        for(int i=0;i<n;i++){
-18            if(cnt0==1 && nums[i]!=0){
-19                ans[i]=0;
-20            }
-21            else if(nums[i]==0){
-22                ans[i]=pro;
-23            }else{
-24                ans[i]=pro/nums[i];
-25            }
-26        }
-27        return ans;
-28    }
-29};
+4        int n=nums.size();
+5        vector<int> pre(n,1);
+6        for(int i=1;i<n;i++){
+7            pre[i]=pre[i-1]*nums[i-1];
+8        }
+9
+10        vector<int> suf(n,1);
+11        for(int i=n-2;i>=0;i--){
+12            suf[i]=suf[i+1]*nums[i+1];
+13        }
+14
+15        vector<int> ans(n);
+16        for(int i=0;i<n;i++){
+17            ans[i]=pre[i]*suf[i];
+18        }
+19        return ans;
+20    }
+21};
