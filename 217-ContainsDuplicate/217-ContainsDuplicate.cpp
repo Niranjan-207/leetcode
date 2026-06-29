400f4e1a89efb4ca89339af0900e80c1d7efe923
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-06-29 20:30:28 +0530

    File updated at 6/29/2026, 8:30:28 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.cpp
+++ 217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -1,10 +1,12 @@
-// Last updated: 4/4/2026, 11:05:48 PM
-class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-        set<int> se;
-        for(int num:nums)   se.insert(num);
-        if(nums.size()==se.size())  return false;
-        return true;
-    }
-};
+// Last updated: 6/29/2026, 8:30:28 PM
+1class Solution {
+2public:
+3    bool containsDuplicate(vector<int>& nums) {
+4        set<int> se;
+5        for(int num:nums){
+6            if(se.count(num))   return true;
+7            else    se.insert(num);
+8        }
+9        return false;
+10    }
+11};
