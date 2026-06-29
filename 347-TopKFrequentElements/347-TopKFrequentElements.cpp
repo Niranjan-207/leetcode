ee5463290677eb0cfd4edbdbbb874154d2c92cf0
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-06-29 23:05:40 +0530

    File updated at 6/29/2026, 11:05:40 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.cpp
+++ 347-TopKFrequentElements/347-TopKFrequentElements.cpp
@@ -1,20 +1,20 @@
-// Last updated: 4/4/2026, 11:05:07 PM
-class Solution {
-public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        for(int& num:nums)  mp[num]++;
-        vector<pair<int,int>> vec;
-        
-        for(auto& [num,freq]:mp){
-            vec.push_back({freq,num});
-        }
-        sort(vec.rbegin(),vec.rend());
-        vector<int> ans;
-        for(int i=0;i<vec.size();i++){
-            if(ans.size()==k)   break;
-            ans.push_back(vec[i].second);
-        }
-        return ans;
-    }
-};
+// Last updated: 6/29/2026, 11:05:40 PM
+1class Solution {
+2public:
+3    vector<int> topKFrequent(vector<int>& nums, int k) {
+4        unordered_map<int,int> mp;
+5        for(int num:nums)   mp[num]++;
+6
+7        vector<pair<int,int>> arr;
+8        for(auto& [num,fre]:mp){
+9            arr.push_back({fre,num});
+10        }
+11        sort(rbegin(arr),rend(arr));
+12        vector<int> ans;
+13        int i=0;
+14        while(k--){
+15            ans.push_back(arr[i++].second);
+16        }
+17        return ans;
+18    }
+19};
