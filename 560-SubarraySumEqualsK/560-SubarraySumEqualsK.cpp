0649a9d68d5ac28d5dd8c87b218df0e61c59430a
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-06-30 19:33:30 +0530

    File updated at 6/30/2026, 7:33:29 PM

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
@@ -1,17 +1,19 @@
-// Last updated: 4/4/2026, 11:04:26 PM
-class Solution {
-public:
-    int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        mp[0]=1;
-        int sum=0,cnt=0;
-        for(int num:nums){
-            sum+=num;
-            if(mp.find(sum-k)!=mp.end()){
-                cnt+=mp[sum-k];
-            }
-            mp[sum]++;
-        }
-        return cnt;
-    }
-};
+// Last updated: 6/30/2026, 7:33:29 PM
+1class Solution {
+2public:
+3    int subarraySum(vector<int>& nums, int k) {
+4        unordered_map<int,int> mp;
+5        mp[0]=1;
+6        int sum=0;
+7        int cnt=0;
+8        for(int num:nums){
+9            sum+=num;
+10            int need=sum-k;
+11            if(mp.contains(need)){
+12                cnt+=mp[need];
+13            }
+14            mp[sum]++;
+15        }
+16        return cnt;
+17    }
+18};
