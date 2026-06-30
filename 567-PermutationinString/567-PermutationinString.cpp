a1953d028145609830223d228bd79d317b79249d
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-06-30 12:09:50 +0530

    File updated at 6/30/2026, 12:09:49 PM

--- 567-PermutationinString/567-PermutationinString.cpp
+++ 567-PermutationinString/567-PermutationinString.cpp
@@ -1,27 +1,28 @@
-// Last updated: 6/30/2026, 12:05:15 PM
+// Last updated: 6/30/2026, 12:09:49 PM
 1class Solution {
 2public:
 3    bool checkInclusion(string s1, string s2) {
-4        
-5        int n=s1.length();
-6        int m=s2.length();
-7        if(m<n) return false;
-8
-9        vector<int> f1(26,0),f2(26,0);
-10        for(char c:s1){
-11            f1[c-'a']++;
-12        }
-13        int i=0;
-14        while(i<n){
-15            f2[s2[i]-'a']++;
-16            i++;
-17        }
-18        if(f1==f2)  return true;
-19        for(;i<m;i++){
-20            f2[s2[i-n]-'a']--;
-21            f2[s2[i]-'a']++;
-22            if(f1==f2)  return true;
-23        }
+4        int n=s1.length(),m=s2.length();
+5        if(n>m) return false;
+6
+7        vector<int> s1f(26,0),s2f(26,0);
+8        for(int i=0;i<n;i++){
+9            s1f[s1[i]-'a']++;
+10            s2f[s2[i]-'a']++;
+11        }
+12        
+13        
+14        if(s1f==s2f)    return true;
+15
+16         for(int i=n;i<m;i++){
+17            s2f[s2[i]-'a']++;
+18            s2f[s2[i-n]-'a']--;
+19
+20        
+21            if(s1f==s2f)    return true;
+22         }
+23        
 24        return false;
-25    }
-26};
+25
+26    }
+27};
