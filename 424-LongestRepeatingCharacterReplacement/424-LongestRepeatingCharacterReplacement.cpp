dbf3696eb3bc75c620772ff5de1f70f025bd02d8
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-06-30 12:25:42 +0530

    File updated at 6/30/2026, 12:25:41 PM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
@@ -1,22 +1,21 @@
-// Last updated: 4/4/2026, 11:04:52 PM
-class Solution {
-public:
-
-    int characterReplacement(string s, int k) {
-        int len=0,n=s.length(),mf=0;
-        int l=0;
-        unordered_map<char,int> mp;
-        for(int r=0;r<n;r++){
-            mp[s[r]]++;
-            mf=max(mf,mp[s[r]]);
-            //mf=maxfre(mp);
-            if((r-l+1)-mf > k){
-                mp[s[l]]--;
-                l++;
-                //mf=maxfre(mp);
-            }
-            len=max(len,r-l+1);
-        }
-        return len;
-    }
-};
+// Last updated: 6/30/2026, 12:25:41 PM
+1class Solution {
+2public:
+3    int characterReplacement(string s, int k) {
+4        int n=s.length();
+5        unordered_map<char,int> mp;
+6        int l=0;
+7        int mf=0;
+8        int ans=0;
+9        for(int r=0;r<n;r++){
+10            mp[s[r]]++;
+11            mf=max(mf,mp[s[r]]);
+12            while((r-l+1)-mf > k){
+13                mp[s[l]]--;
+14                l++;
+15            }
+16            ans=max(ans,r-l+1);
+17        }
+18        return ans;
+19    }
+20};
