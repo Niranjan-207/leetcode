6c4d4322b9252e948534a60c3438f6f6d67657ba
Niranjan-207 <159038348+Niranjan-207@users.noreply.github.com>
2026-06-30 11:07:36 +0530

    File updated at 6/30/2026, 11:07:36 AM

--- 206-ReverseLinkedList/206-ReverseLinkedList.cpp
+++ 206-ReverseLinkedList/206-ReverseLinkedList.cpp
@@ -1,31 +1,29 @@
-// Last updated: 4/4/2026, 11:05:55 PM
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-        if(head==nullptr || head->next==nullptr){
-            return head;
-        }
-        ListNode* f=nullptr;
-        ListNode* s=head;
-        
-        while(s!=nullptr){
-            ListNode* t=s->next;
-            s->next=f;
-            f=s;
-            s=t;
-        }
-
-        return f;
-        
-    }
-};
+// Last updated: 6/30/2026, 11:07:36 AM
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* reverseList(ListNode* head) {
+14        if(!head || !head->next)    return head;
+15
+16        ListNode* p=nullptr;
+17        ListNode* cur=head;
+18        ListNode* n=nullptr;
+19
+20        while(cur!=nullptr){
+21            n=cur->next;
+22            cur->next=p;
+23            p=cur;
+24            cur=n;
+25        }
+26        return p;
+27    }
+28};
